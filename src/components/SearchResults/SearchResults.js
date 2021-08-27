7f54c29e3c89f3b045485b724e735a9d045f4c4f
Bobby Vasquez <shootbv@gmail.com>
2021-08-27 15:04:05 -0600

    Add basic functionality for userbase search

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,7 +1,12 @@
 const SearchResults = ({results}) => {
+  let parsed;
+  if (results) {
+    parsed = results.map(result => <article>{result.first_name} {result.last_name}</article>)
+  }
+
   return (
     <section>
-      {!results ? null : results}
+      {!results ? null : parsed}
     </section>
   )
 }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,17 +1,39 @@
+
+import React, { useState, useEffect } from 'react'
 import Feed from '../../components/Feed/Feed.js'
 import SearchBar from '../../components/SearchBar/SearchBar.js'
 import SearchResults from '../../components/SearchResults/SearchResults.js'
 import NavBar from '../../components/NavBar/NavBar.js'
 import Header from '../../components/Header/Header.js'
+import { users, posts } from '../../utilities/mockData'
 
 import './App.css';
 
 const App = () => {
+
+  const [userbase, setUsers] = useState([])
+  const [query, setQuery] = useState('')
+
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) setUsers(users.data.attributes)
+    return () => mounted = false;
+  }, [])
+
+
+  const queryResults = (data) => {
+    return userbase.users.filter(user => (user.username.includes(query)) || (user.first_name.includes(query)) || (user.last_name.includes(query)))
+  }
+  // const queryUsers = (query) => {
+  //   let results = userbase.filter(user => (user.username.includes(query)) || (user.first_name.includes(query)) || (user.last_name.includes(query)))
+  //   return (results) ? results : "No matches for query - please try another"
+  // }
+
   return (
     <main>
       <Header />
-      <SearchBar />
-      <SearchResults />
+      <SearchBar query={query} set={setQuery} />
+      <SearchResults results={!query ? null : queryResults(query)}/>
       <Feed />
       <NavBar />
     </main>
