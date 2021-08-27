3b8cd8e856093e99a9deface1faa6bc88b3d5942
Bobby Vasquez <shootbv@gmail.com>
2021-08-27 14:43:49 -0600

    Add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Seen</h1>
+    </header>
+  )
+}
+
+export default Header
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,13 +1,17 @@
 import Feed from '../../components/Feed/Feed.js'
 import SearchBar from '../../components/SearchBar/SearchBar.js'
+import SearchResults from '../../components/SearchResults/SearchResults.js'
 import NavBar from '../../components/NavBar/NavBar.js'
+import Header from '../../components/Header/Header.js'
 
 import './App.css';
 
 const App = () => {
   return (
     <main>
+      <Header />
       <SearchBar />
+      <SearchResults />
       <Feed />
       <NavBar />
     </main>
