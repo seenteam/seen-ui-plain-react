49f04d4c9639ed9426655505909417d2e3ac22b9
Bobby Vasquez <shootbv@gmail.com>
2021-08-27 14:19:04 -0600

    Create basic setup for NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <footer>
+      <ul>
+        <li>Feed</li>
+        <li>Search</li>
+        <li>Flux</li>
+        <li>Profile</li>
+      </ul>
+    </footer>
+  )
+}
+
+export default NavBar
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,11 @@
 import Feed from '../../components/Feed/Feed.js'
+import SearchBar from '../../components/SearchBar/SearchBar.js'
 import './App.css';
 
 const App = () => {
   return (
     <main>
+    <SearchBar />
       <Feed />
     </main>
   );
