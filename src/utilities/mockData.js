73b89809fb909fc9282c2fc173a70828748e24ab
Bobby Vasquez <shootbv@gmail.com>
2021-08-27 14:27:04 -0600

    Add mock data for users

---
+++ src/utilities/mockData.js
@@ -0,0 +1,34 @@
+const users = {
+    "data": {
+        "id": null,
+        "type": "users",
+        "attributes": {
+            "users": [
+                {
+                "id": 1,
+                "username": "exaplename",
+                "first_name": "firstname",
+                "last_name": "lastname",
+                "email": "example@example.com",
+                "phone_number": "999-999-9999",
+                "birthday": "1-11-1990",
+                "created_at": '8-26-2021',
+                "updated_at": '8-26-2021'
+                },
+                {
+                "id": 2,
+                "username": "exaplename",
+                "first_name": "firstname",
+                "last_name": "lastname",
+                "email": "example2@example.com",
+                "phone_number": "999-999-9999",
+                "birthday": "1-11-1990",
+                "created_at": '8-26-2021',
+                "updated_at": '8-26-2021'
+                }
+            ]
+        }
+    }
+}
+
+const posts;
