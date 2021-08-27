344eb105bbc73742bdf4550f4143f763cfb1e1ab
Bobby Vasquez <shootbv@gmail.com>
2021-08-27 14:22:43 -0600

    Add light styling to NavBar

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,15 @@
 import Feed from '../../components/Feed/Feed.js'
 import SearchBar from '../../components/SearchBar/SearchBar.js'
+import NavBar from '../../components/NavBar/NavBar.js'
+
 import './App.css';
 
 const App = () => {
   return (
     <main>
-    <SearchBar />
+      <SearchBar />
       <Feed />
+      <NavBar />
     </main>
   );
 }
