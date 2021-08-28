fcf7ae4ef656b5592d0a51d49affe6b29e32f4a2
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-28 14:39:00 -0600

    Add exchange rates example for graphQL

---
+++ src/components/ExchangeRates/ExchangeRates.js
@@ -0,0 +1,20 @@
+import { useQuery} from "@apollo/client";
+import { EXCHANGE_RATES } from '../../queries/queries';
+
+ const ExchangeRates = () => {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+  
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+  
+    return data.rates.map(({ currency, rate }) => (
+      <div key={currency}>
+        <p>
+          {currency}: {rate}
+        </p>
+      </div>
+    ));
+  };
+
+export default ExchangeRates;
+  
