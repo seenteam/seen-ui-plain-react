4cd86a902656fc4fac94d21555c0b957ff0fbacf
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-28 14:40:42 -0600

    Add GQL query to get user info

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -10,6 +10,16 @@ export const EXCHANGE_RATES = gql`
     }
   }
 `;
-
+ 
+export const GET_USER_INFO = (id) => gql`
+query GetUserInfo {
+  user_id(id: ${id}) {
+    name
+    posts
+    fixed_followers
+    flux_followers
+  }
+}
+`;
 
 //Add more queries below!
