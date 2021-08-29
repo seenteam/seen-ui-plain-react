d09b0ffa12374f82bad40ef98c95c1526d8baa55
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-29 15:25:14 -0600

    Make profile details pg! Displays user info

---
+++ src/components/ProfileDetails/ProfileDetails.js
@@ -0,0 +1,71 @@
+import { useQuery} from "@apollo/client";
+import { GET_ALL_USERS, GET_ALL_USER_INFO } from '../../queries/queries';
+
+/*
+users {
+    id
+    userName
+    firstName
+    lastName 
+    email
+    phoneNumber
+    birthday
+    posts{
+      content
+			}
+  }
+
+*/
+
+const DisplayUserData = () => {
+   const { loading, error, data } = useQuery(GET_ALL_USER_INFO);
+
+   if (loading) return <p>Loading...</p>;
+   if (error) return <p>Error :(</p>;
+
+    const check = () => {
+      console.log(data)
+    }
+    check()
+
+   return data.users.map(({ 
+     firstName,
+     lastName,
+     id,
+     userName,
+     email,
+     phoneNumber,
+     birthday,
+     posts 
+     }) => (
+     <div key={id}>
+
+       <p>
+         Username : {userName}
+       </p>
+
+       <p>
+         name : {firstName} {lastName}
+       </p>
+
+       <p>
+        email: {email}
+       </p>
+       
+       <p>
+        Phone Number : {phoneNumber}
+       </p>
+
+       <p>
+        Birthday: {birthday}
+       </p>
+
+        {/* <p>
+         posts:  {posts.map(post => post.content)}
+        </p> */}
+     </div>
+   ));
+ };
+
+export default DisplayUserData;
+  
