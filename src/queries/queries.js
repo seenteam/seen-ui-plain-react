467b1e4fb16eacf892100c8a588153adb16739a8
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-08-29 15:23:55 -0600

    Update the queries

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -2,15 +2,17 @@ import {
     gql
   } from "@apollo/client";
 
-export const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
+// export const EXCHANGE_RATES = gql`
+//   query GetExchangeRates {
+//     rates(currency: "USD") {
+//       currency
+//       rate
+//     }
+//   }
+// `;
  
+
+//Probably not going to work, first attempt at queries
 export const GET_USER_INFO = (id) => gql`
 query GetUserInfo {
   userId(id: ${id}) {
@@ -23,7 +25,7 @@ query GetUserInfo {
 `;
 
 
-// 
+//Probably not going to work, first attempt at queries
 export const GET_POSTS_FROM_FRIENDS = (id) => gql`
 query GetPostsFromFriends {
   userId(id: ${id}) {
@@ -41,6 +43,8 @@ query GetPostsFromFriends {
 }
 `;
 
+
+//TESTed this query it works!
 export const GET_ALL_USERS = gql`
 query {
   users {
@@ -49,5 +53,20 @@ query {
   }
 }
 `
-
-//Add more queries below!
+//Tested this query it also works!
+export const GET_ALL_USER_INFO = gql`
+query {
+	users {
+    id
+    userName
+    firstName
+    lastName 
+    email
+    phoneNumber
+    birthday
+    posts{
+      content
+			}
+  }
+}
+`
