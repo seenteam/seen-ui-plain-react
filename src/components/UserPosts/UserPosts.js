9a1e6f20f615ba078c8a4ece119b6214b07ac1aa
Bobby Vasquez <shootbv@gmail.com>
2021-08-30 16:42:04 -0600

    Add dynamic functionality to GET_USER_POSTS query

--- src/components/UserPosts/UserPosts.js
+++ src/components/UserPosts/UserPosts.js
@@ -1,8 +1,11 @@
+import React, {useState} from 'react'
 import { useQuery} from "@apollo/client";
 import { GET_USER_POSTS } from '../../queries/queries';
 
 const UserPosts = () => {
-  const { loading, error, data } = useQuery(GET_USER_POSTS(2));
+  const [query, setQuery] = useState(1)
+  const { loading, error, data } = useQuery(GET_USER_POSTS(query));
+
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
@@ -11,8 +14,21 @@ const UserPosts = () => {
     console.log(data)
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    setQuery(e.target.value || 1)
+    return query
+  }
+
    return  (
      <div>
+     <form onSubmit={handleSubmit}>
+     <input
+      value={query}
+      placeholder="User ID"
+      onChange={handleSubmit}
+     />
+     </form>
         {data.user.posts.map(post => <p>{post.content}</p>)}
      </div>
    )
