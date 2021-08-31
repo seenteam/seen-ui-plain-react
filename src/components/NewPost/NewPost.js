729e842e3c3596c5380b9af45e99b1d8d3a093be
Bobby Vasquez <shootbv@gmail.com>
2021-08-31 15:38:39 -0600

    Add functionality to add posts for users

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,16 +1,47 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { useMutation } from "@apollo/client";
+import * as gql from '../../queries/queries';
+
 import './NewPost.css'
 
-const NewPost = ({visible, setVisibility, posts, setPosts}) => {
+const NewPost = ({visible, setVisibility, data, setPosts, userID }) => {
 
+  // const [fetched, setFetched] = useState('')
   const [postData, setPostData] = useState('')
   // const [posts, setPosts] = useState([])
+  const [createPost] = useMutation(gql.CREATE_POST, {
+    refetchQueries: [{ query: gql.GET_USER_POSTS(userID) }],
+  });
+  // const check = (post) => {
+  //   console.log('DATA BEING PASSED', data, 'NEW DATA', post)
+  //   console.log('WHAT I WANT', [...fetched, post])
+  // }
+
+  // useEffect(() => {
+  //   let mounted = true;
+  //   if (mounted) {
+  //     if (data) setFetched(data)
+  //   }
+  //   return () => mounted = false;
+  // }, [])
 
   const submitForm = e => {
     e.preventDefault()
-    setPosts([...posts, postData])
+    // setPosts([...posts, postData])
     setVisibility(false)
+    let newPost = {
+      content: postData,
+      userId: userID
+    }
+    createPost({
+      variables: {
+        content: postData,
+        userId: userID
+      },
+    })
+    // setPosts([...data, newPost])
     setPostData('')
+    // check(newPost)
   }
 
   return (
