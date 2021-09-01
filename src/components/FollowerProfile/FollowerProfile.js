8c2c6aef46f9531832e005951e60079ffdd153b0
Bobby Vasquez <shootbv@gmail.com>
2021-09-01 15:59:24 -0600

    Add component to render FollowerProfile

---
+++ src/components/FollowerProfile/FollowerProfile.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react'
+import { useQuery, useMutation } from "@apollo/client";
+import FollowerDetails from '../FollowerDetails/FollowerDetails'
+import * as gql from '../../queries/queries'
+
+const FollowerProfile = ({user}) => {
+  const { loading, error, data } = useQuery(gql.GET_USER_INFO(user));
+  const [profile, setProfile] = useState('')
+  useEffect(() => {
+    let mounted = true;
+    if (mounted && data) check()
+    return () => mounted = false;
+  })
+
+  const check = () => {
+    console.log(data)
+  }
+
+  const renderPosts = (posts) => {
+    return (
+      <ul>
+        {posts.map(post => <li>{post.content}</li>)}
+      </ul>
+    )
+  }
+
+  const renderFollowers = (followers) => {
+    return (
+      <div>
+        {followers.map(follower => <FollowerDetails id={follower.friendId}/>)}
+      </div>
+    )
+  }
+
+  const renderProfile = () => {
+    return <div>
+      <h2>{data.user.firstName} {data.user.lastName}</h2>
+      <h3>{data.user.userName}</h3>
+      <h4>Followers</h4>
+      {renderFollowers(data.user.followers)}
+      {renderPosts(data.user.posts)}
+    </div>
+  }
+
+  return (
+    <section>
+      <h1>Profile</h1>
+      {!!data && renderProfile()}
+    </section>
+  )
+
+
+
+}
+
+export default FollowerProfile
