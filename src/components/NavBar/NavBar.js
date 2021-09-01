b1bdb0475fcea7a68a1f2dfd1052f1911b785c9e
Bobby Vasquez <shootbv@gmail.com>
2021-09-01 16:00:11 -0600

    Add exact parameter to Link components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,11 +10,11 @@ const NavBar = ({newPost, setNewPost}) => {
   return (
     <footer>
       <ul>
-        <Link to="/"><li><img src="https://img.icons8.com/wired/64/000000/activity-feed-2.png"/></li></Link>
-        <Link to="/search-page"><li><img src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"/></li></Link>
+        <Link exact to="/"><li><img src="https://img.icons8.com/wired/64/000000/activity-feed-2.png"/></li></Link>
+        <Link exact to="/search-page"><li><img src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"/></li></Link>
         <li className={(!newPost) ? 'new-post' : 'yellow'} onClick={togglePost}><img src="https://img.icons8.com/wired/64/000000/plus.png"/></li>
-        <Link to="followers"><li><img src="https://img.icons8.com/dotty/80/000000/sine.png"/></li></Link>
-        <Link to="/profile"><li><img src="https://img.icons8.com/wired/64/000000/circled-user.png"/></li></Link>
+        <Link exact to="/followers"><li><img src="https://img.icons8.com/dotty/80/000000/sine.png"/></li></Link>
+        <Link exact to="/profile"><li><img src="https://img.icons8.com/wired/64/000000/circled-user.png"/></li></Link>
       </ul>
     </footer>
   )
