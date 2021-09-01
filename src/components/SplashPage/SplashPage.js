437dedf89d7c606b687e863f28d176266e692ea2
Bobby Vasquez <shootbv@gmail.com>
2021-09-01 16:02:44 -0600

    Update SplashPage component to render user name

--- src/components/SplashPage/SplashPage.js
+++ src/components/SplashPage/SplashPage.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import Feed from '../Feed/Feed.js'
 import Header from '../Header/Header.js'
+import { useQuery } from '@apollo/client'
+import * as gql from '../../queries/queries'
 
-const SplashPage = ({login, setNewPost}) => {
+const SplashPage = ({login, setNewPost, id}) => {
 
+  const { data, error } = useQuery(gql.GET_USER_NAME(id))
   useEffect(() => {
     setNewPost(false);
   }, [])
@@ -11,7 +14,9 @@ const SplashPage = ({login, setNewPost}) => {
   return (
     <main>
       <Header />
-      <Feed />
+      <h1>{!!data && `Welcome ${data.user.firstName} ${data.user.lastName}`}</h1>
+      {!!error && console.log(error)}
+    <Feed />
     </main>
   )
 }
