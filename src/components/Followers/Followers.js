5fddf3cd611f631ae06f89d9d9870883677e1a18
Bobby Vasquez <shootbv@gmail.com>
2021-09-01 16:05:10 -0600

    Change query to render from - previously GET_FOLLOWERS_INFO

--- src/components/Followers/Followers.js
+++ src/components/Followers/Followers.js
@@ -1,33 +1,56 @@
+import { Link } from 'react-router-dom'
 import { useQuery, useMutation } from "@apollo/client";
+import { useState, useEffect } from "react";
+import FollowerDetails from '../FollowerDetails/FollowerDetails'
+
 import * as gql from '../../queries/queries'
 
 const Followers = ({id}) => {
 
-  const { loading, error, data } = useQuery(gql.GET_FOLLOWING_INFO(id))
+  const { loading, error, data } = useQuery(gql.GET_USER_INFO(id))
   const [createFollower] = useMutation(gql.CREATE_FOLLOWER, {
-    refetchQueries: [{ query: gql.GET_FOLLOWING_INFO(id) }],
+    refetchQueries: [{ query: gql.GET_USER_INFO(id) }],
   });
+
+  const [user, setUser] = useState('')
+
+  // useEffect(() => {
+  //   let mounted = true;
+  //   if (mounted && data) setUser({followers: data.usersFollowers, id})
+  //   return () => mounted = false;
+  // }, [])
 
   const check = () => {
     if (data) console.log(data)
   }
 
+  const renderFollowers = () => {
+    if (data) return data.user.followers.map(follower => {
+      return <div>
+        <FollowerDetails id={follower.friendId} />
+      </div>
+    })
+  }
+
   const addFriend = (e) => {
-    let idToAdd = 4
-    if (idToAdd === id) return e.target.innerText = 'Cant add yourself!'
+    // let idToAdd = 4
+    // if (idToAdd === id) return e.target.innerText = 'Cant add yourself!'
     e.target.setAttribute('disabled', true)
+    check()
     createFollower({
       variables: {
-        'userId': 2,
-        'friendId': id
+        'userId': id,
+        'friendId': 1
       },
     })
   }
 
   return (
     <div>
-      {!!data && check()}
-      {<button onClick={e => addFriend(e)}>Add friend 2</button>}
+    <h2>Fixed Followers</h2>
+      <p>Current user: ID{id}</p>
+      {!!data && renderFollowers()}
+      {<button onClick={e => addFriend(e)}>Add friend 4</button>}
     </div>
   )
 }
