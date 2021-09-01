ab9c0ec2f1a3f23dbafcb9b8ae127ff496914bb2
Bobby Vasquez <shootbv@gmail.com>
2021-09-01 16:28:21 -0600

    Update UserProfile to render followersDetails by click

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react'
 import { useQuery, useMutation } from "@apollo/client";
 import FollowerDetails from '../FollowerDetails/FollowerDetails'
 import * as gql from '../../queries/queries'
+import './UserProfile.css'
 
 const UserProfile = ({user}) => {
   const { loading, error, data } = useQuery(gql.GET_USER_INFO(user));
   const [profile, setProfile] = useState('')
+  const [followersVisible, setFollowersVisible] = useState(false)
   useEffect(() => {
     let mounted = true;
     if (mounted && data) check()
@@ -26,25 +28,35 @@ const UserProfile = ({user}) => {
 
   const renderFollowers = (followers) => {
     return (
-      <div>
+      <div className={!followersVisible ? "followers-list inactive" : "followers-list"}>
+        <button onClick={() => setFollowersVisible(false)}>Close</button>
         {followers.map(follower => <FollowerDetails id={follower.friendId}/>)}
       </div>
     )
   }
 
+  const revealFollowers = () => {
+    setFollowersVisible(true)
+  }
+
   const renderProfile = () => {
     return <div>
-      <h2>{data.user.firstName} {data.user.lastName}</h2>
-      <h3>{data.user.userName}</h3>
-      <h4>Followers</h4>
-      {renderFollowers(data.user.followers)}
+      <section className="profile-header">
+        <div>
+        <h2>{data.user.firstName} {data.user.lastName}</h2>
+        <h3>{data.user.userName}</h3>
+        </div>
+        <div className="followers-info">
+          <h4 onClick={revealFollowers}>{data.user.followers.length} Followers</h4>
+          {renderFollowers(data.user.followers)}
+        </div>
+      </section>
       {renderPosts(data.user.posts)}
     </div>
   }
 
   return (
     <section>
-      <h1>Profile</h1>
       {!!data && renderProfile()}
     </section>
   )
