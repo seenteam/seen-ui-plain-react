79bc5a5605ef509e8415b9fbeb363b6e14b856c8
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-02 17:18:31 -0600

    Complete updateUser functionality

--- src/components/UpdateUserData/UpdateUserData.js
+++ src/components/UpdateUserData/UpdateUserData.js
@@ -4,41 +4,46 @@ import React from "react";
 import { CREATE_NEW_USER } from "../../queries/queries";
 
 
+
 const UpdateUserData = () => {
     let userName, firstName, lastName, phoneNumber, email, birthday;
-    const [createUser] =  useMutation(CREATE_NEW_USER);
-
+    //update create NEW USER
+    const [updateUser] =  useMutation(gql.CREATE_NEW_USER);
+    
+    const updatingUser = (e) => {
+            console.log("Updating USER")
+            e.preventDefault();
+    
+            updateUser( 
+                {variables: {userName: userName.value, firstName:firstName.value, lastName: lastName.value,
+                     phoneNumber: phoneNumber.value, email: email.value, birthday: birthday.value 
+                }})
+    }
+    
     return (
         <section>
-            <form onSubmit={ e => {
-                e.preventDefault();
-
-                createUser( 
-                    {variables: {userName: userName.value, firstName:firstName.value, lastName: lastName.value, phoneNumber: phoneNumber.value, email: email.value, birthday: birthday.value 
-                    }})
-            }}>
-                <label for="userName">Username: </label>
+            <form onSubmit={updatingUser}>
+                <label htmlFor="userName">Username: </label>
                 <input ref={value => userName = value} id='userName'></input> <br/>
 
-                <label for="firstName">First name: </label>
+                <label htmlFor="firstName">First name: </label>
                 <input ref={value => firstName = value} id='firstName'></input> <br/>
 
-                <label for="lastName">Last name: </label>
+                <label htmlFor="lastName">Last name: </label>
                 <input ref={value => lastName = value} id='lastName'></input> <br/>
 
-                <label for="phoneNumber">Phone Number: </label>
+                <label htmlFor="phoneNumber">Phone Number: </label>
                 <input ref={value => phoneNumber = value} id='phoneNumber'></input> <br/>
 
-                <label for="email">Email address:</label>
+                <label htmlFor="email">Email address:</label>
                 <input ref={value => email = value} id='email'></input> <br />
 
-                <label for="birthday">Birth date: </label>
+                <label htmlFor="birthday">Birth date: </label>
                 <input ref={value => birthday = value} id='birthday'></input> <br/>
 
-                <button type="submit">Create User</button>
+                <button onClick={updatingUser}> Update User</button>
             </form>
         </section>
     )
 }
-
 export default UpdateUserData;
