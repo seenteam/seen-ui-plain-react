9a2e8e4ec662e04f7160f53c45ec46cb166ce036
Bobby Vasquez <shootbv@gmail.com>
2021-09-03 16:04:31 -0600

    Turn newPost li into button for tabbable functionality

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,12 +7,16 @@ const NavBar = ({newPost, setNewPost}) => {
     (!newPost) ? setNewPost(true) : setNewPost(false)
   }
 
+  const handleKeypress = e => {
+    if (e.keyCode === 13) togglePost();
+};
+
   return (
     <footer>
       <ul>
         <Link exact to="/"><li><img src="https://img.icons8.com/wired/64/000000/activity-feed-2.png"/></li></Link>
         <Link exact to="/search-page"><li><img src="https://img.icons8.com/pastel-glyph/64/000000/search--v2.png"/></li></Link>
-        <li className={(!newPost) ? 'new-post' : 'yellow'} onClick={togglePost}><img src="https://img.icons8.com/wired/64/000000/plus.png"/></li>
+        <li className={(!newPost) ? 'new-post' : 'yellow'}><button onClick={togglePost} onKeyPress={handleKeypress}><img src="https://img.icons8.com/wired/64/000000/plus.png"/></button></li>
         <Link exact to="/followers"><li><img src="https://img.icons8.com/dotty/80/000000/sine.png"/></li></Link>
         <Link exact to="/profile"><li><img src="https://img.icons8.com/wired/64/000000/circled-user.png"/></li></Link>
       </ul>
