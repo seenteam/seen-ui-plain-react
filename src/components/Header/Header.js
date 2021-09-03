f9b1db0c5be780f0e7ac4b5100333c42d4198654
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-03 17:22:05 -0600

    Move splash page pieces into header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,21 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/client'
 import './Header.css'
+import * as gql from '../../queries/queries'
 
-const Header = () => {
+
+const Header = ({setNewPost, id}) => {
+
+ const { data, error } = useQuery(gql.GET_USER_NAME(id))
+  useEffect(() => {
+    setNewPost(false);
+  }, [])
+
   return (
     <header>
       <h1>Seen</h1>
+      <h1>{!!data && `Welcome ${data.user.firstName} ${data.user.lastName}`}</h1>
+      {!!error && console.log(error)}
     </header>
   )
 }
