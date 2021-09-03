171818728f058062b7390801485a84183853a8c3
Bobby Vasquez <shootbv@gmail.com>
2021-09-03 16:15:48 -0600

    Add separate component for FollowersList

---
+++ src/components/FollowersList/FollowersList.js
@@ -0,0 +1,30 @@
+import FollowerDetails from '../FollowerDetails/FollowerDetails'
+import './FollowersList.css'
+
+const FollowersList = ({ followers, visible, setVisible}) => {
+
+  const toggleList = () => {
+    (!visible) ? setVisible(true) : setVisible(false)
+  }
+
+  const renderList = () => {
+    return (
+      <div>
+      {followers.map((follower, index) => <FollowerDetails key={index} id={follower.friendId} followersVisible={setVisible}/>)}
+      </div>
+    )
+  }
+
+  return (
+    <section className={!visible ? "follow-list hidden" : "follow-list"}>
+      {
+        !!visible && <div>
+        <button onClick={toggleList}>Close</button>
+        {renderList()}
+      </div>
+      }
+    </section>
+  )
+}
+
+export default FollowersList
