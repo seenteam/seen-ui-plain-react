486993cc6835f354832704d79cc46790cd3db2bd
Bobby Vasquez <shootbv@gmail.com>
2021-09-03 16:11:54 -0600

    Add further styling to Post component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,6 +1,18 @@
-const Post = () => {
+import './Post.css'
+const dayjs = require('dayjs')
+const LocalizedFormat = require('dayjs/plugin/localizedFormat')
+dayjs.extend(LocalizedFormat)
+
+const Post = ({content, created}) => {
   return (
-    <div></div>
+    <div className="post-container">
+      <p><strong>{content}</strong></p>
+      <p>Posted {dayjs(created).format('LLL')}</p>
+      <div className="likes-container">
+        <p># Likes</p>
+        <button>Like</button>
+      </div>
+    </div>
   )
 }
 
