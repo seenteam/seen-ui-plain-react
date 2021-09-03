29c11b71026ad24ac2b9632eb19bdb9fe5abf5ba
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-03 17:24:53 -0600

    Cleanup  profile child components

--- src/components/ProfileDetails/ProfileDetails.js
+++ src/components/ProfileDetails/ProfileDetails.js
@@ -1,25 +1,7 @@
 import { useQuery} from "@apollo/client";
-import React, { useEffect } from 'react';
+import React from 'react';
 import './ProfileDetails.css'
 import * as gql from '../../queries/queries';
-
-//import { GET_ALL_USERS, GET_ALL_USER_INFO } from '../../queries/queries';
-
-/*
-users {
-    id
-    userName
-    firstName
-    lastName 
-    email
-    phoneNumber
-    birthday
-    posts{
-      content
-			}
-  }
-
-*/
 
 const ProfileDetails = ({userID}) => {
    const { loading, error, data } = useQuery(gql.GET_USER_INFO(userID));
@@ -27,19 +9,6 @@ const ProfileDetails = ({userID}) => {
    if (loading) return <p>Loading...</p>;
    if (error) return <p>Error :(</p>;
 
-    const check = () => {
-      console.log({data})
-    }
-    check()
-
-    // firstName,
-    //  lastName,
-    //  id,
-    //  userName,
-    //  email,
-    //  phoneNumber,
-    //  birthday,
-    //  posts 
 
    return (
     <section>
