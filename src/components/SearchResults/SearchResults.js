1166b0f46bc18b6d334c94c693c2d703b296eeab
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-03 16:03:42 -0600

    Add link for router & reach data with dot notation

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,8 +1,16 @@
 import './SearchResults.css'
+import {Link} from 'react-router-dom'
+
 const SearchResults = ({results}) => {
   let parsed;
   if (results) {
-    parsed = results.map(result => <article key={result.id}>{result.first_name} {result.last_name}</article>)
+    parsed = results.map(result => {
+    return <Link to={`/users/${result.id}`}>
+              <article className='user-container' key={result.id}> 
+                <h4>{result.userName}</h4> 
+                <p> {result.firstName} {result.lastName} </p>
+              </article>
+            </Link>})
   }
 
   return (
