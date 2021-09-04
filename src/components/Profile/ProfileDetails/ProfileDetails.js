edf46f9a5a8d8c7d078bbf2be490849163c66ca9
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-03 21:46:53 -0600

    Use use context to pass down id

--- src/components/Profile/ProfileDetails/ProfileDetails.js
+++ src/components/Profile/ProfileDetails/ProfileDetails.js
@@ -1,38 +1,33 @@
 import { useQuery} from "@apollo/client";
-import React from 'react';
+import React, {useContext} from 'react';
 import './ProfileDetails.css'
 import * as gql from '../../../queries/queries';
+import UserContext from "../../UserProfile/UserContext";
 
-const ProfileDetails = ({userID}) => {
-   const { loading, error, data } = useQuery(gql.GET_USER_INFO(userID));
+const ProfileDetails = () => {
+   const value = useContext(UserContext);
+   const { loading, error, data } = useQuery(gql.GET_USER_INFO(value));
 
    if (loading) return <p>Loading...</p>;
    if (error) return <p>Error :(</p>;
 
-
    return (
     <section>
-
        <p>
          Username : {data.user.userName}
        </p>
-
        <p>
          name :  {data.user.firstName} {data.user.lastName}
        </p>
-       
        <p>
         email: {data.user.email}
        </p>
-
        <p>
         Phone Number : {data.user.phoneNumber}
        </p>
-
        <p>
         Birthday: {data.user.birthday}
        </p>
-
      </section>
    );
  };
