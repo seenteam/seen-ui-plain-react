7ed4c01090c2e79eaec95b70117688080fb7a7d6
Bobby Vasquez <shootbv@gmail.com>
2021-09-04 14:00:37 -0600

    Add react-spinner

--- src/components/Followers/FollowerDetails/FollowerDetails.js
+++ src/components/Followers/FollowerDetails/FollowerDetails.js
@@ -1,15 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { Link } from 'react-router-dom'
 import * as gql from '../../../queries/queries'
+import Loading from '../../Loading/Loading.js'
 import './FollowerDetails.css'
 
 const FollowerDetails = ({id, followersVisible}) => {
   const { loading, error, data } = useQuery(gql.GET_USER_INFO(id))
 
+  if (error) console.log(error, "ERROR!")
+  if (loading) return <Loading loading={loading} type="follower" />
 
   const renderDetails = () => {
-    if (error) console.log(error, "ERROR!")
-    if (loading) return <h2>Loading...</h2>
     return <article>
       <h5>{data.user.firstName} {data.user.lastName}</h5>
     </article>
