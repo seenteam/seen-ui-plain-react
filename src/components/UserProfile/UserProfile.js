ac8708a7deab61bbee7d2d5cf7100a95e99f2b01
Bobby Vasquez <shootbv@gmail.com>
2021-09-04 20:13:17 -0600

    Fix functionality for following/unfollowing a user

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -5,21 +5,22 @@ import Post from '../Post/Post.js'
 import UserContext from "../UserProfile/UserContext";
 import Loading from '../Loading/Loading.js'
 import * as gql from '../../queries/queries'
+
 import './UserProfile.css'
 
 const UserProfile = ({user}) => {
   const value = useContext(UserContext)
-  const GetFollowingInfo = useQuery(gql.GET_FOLLOWING_INFO(value));
-  const GetUserInfo = useQuery(gql.GET_USER_INFO(user));
+  const GetFollowingInfo = useQuery(gql.GET_FOLLOWER_INFO(user));
+  const GetVisitedUserInfo = useQuery(gql.GET_USER_INFO(user));
   const [profile, setProfile] = useState('')
   const [followersVisible, setFollowersVisible] = useState(false)
 
-  const [followerUser] = useMutation(gql.CREATE_FOLLOWER, {
-    refetchQueries: [{ query: gql.GET_FOLLOWING_INFO(value) }, { query: gql.GET_USER_INFO(user) }],
+  const [followUser] = useMutation(gql.CREATE_FOLLOWER, {
+    refetchQueries: [{ query: gql.GET_FOLLOWING_INFO(value) }, { query: gql.GET_FOLLOWER_INFO(user) }, { query: gql.GET_USER_INFO(user) }],
   });
 
   const [unFollowUser] = useMutation(gql.DELETE_FOLLOWER, {
-    refetchQueries: [{ query: gql.GET_FOLLOWING_INFO(value) }, { query: gql.GET_USER_INFO(user) }],
+    refetchQueries: [{ query: gql.GET_FOLLOWING_INFO(value) }, { query: gql.GET_FOLLOWER_INFO(user) }, { query: gql.GET_USER_INFO(user) }, { query: gql.GET_USER_INFO(value) }],
   });
 
   if (GetFollowingInfo.loading) {
@@ -29,14 +30,20 @@ const UserProfile = ({user}) => {
     console.log('Current user following data', GetFollowingInfo.data)
   }
 
+  if (GetVisitedUserInfo.loading) {
+    return <Loading loading={GetFollowingInfo.loading} />
+  }
+  else {
+    console.log('Current user following data', GetFollowingInfo.data)
+  }
   // useEffect(() => {
   //   let mounted = true;
-  //   if (mounted && GetUserInfo.data) check()
+  //   if (mounted && GetVisitedUserInfo.data) check()
   //   return () => mounted = false;
   // })
 
   const check = () => {
-    console.log(GetUserInfo.data)
+    console.log(GetVisitedUserInfo.data)
   }
 
   const renderPosts = (posts) => {
@@ -72,12 +79,14 @@ const UserProfile = ({user}) => {
 
   const renderTest = () => {
     return (
-      <div>{!!GetUserInfo.data && !!GetFollowingInfo.data && renderProfile()}</div>
+      <div>{!!GetVisitedUserInfo.data && !!GetFollowingInfo.data && renderProfile()}</div>
     )
   }
 
   const checkFollowing = () => {
-    let found = GetFollowingInfo.data.following.find(user => user.id === GetUserInfo.data.user.id)
+    console.log('Users following the visited profile', GetFollowingInfo.data.usersFollowers)
+    // console.log('Current user id', GetFollowingInfo.data.usersFollowers)
+    let found = GetFollowingInfo.data.usersFollowers.find(user => user.id === value.toString())
     console.log('Results', found)
     if (found) {
       return true
@@ -94,34 +103,45 @@ const UserProfile = ({user}) => {
   }
 
   const follow = () => {
-
+    console.log('variables', {
+      'userId': value,
+      'friendId': GetVisitedUserInfo.data.user.id
+    })
+    followUser({
+      variables: {
+        'userId': GetVisitedUserInfo.data.user.id,
+        'friendId': value
+      },
+    })
   }
 
   const unfollow = () => {
-    // unFollowUser({
-    //   variables: {
-    //     'postId': postId
-    //   },
+    unFollowUser({
+      variables: {
+        'userId': GetVisitedUserInfo.data.user.id,
+        'followerId': value
+      },
+    })
   }
 
   const renderProfile = () => {
     return <div>
       <section className="profile-header">
         <div>
-        <h2>{GetUserInfo.data.user.firstName} {GetUserInfo.data.user.lastName}</h2>
-        <h3>Username: {GetUserInfo.data.user.userName}</h3>
+        <h2>{GetVisitedUserInfo.data.user.firstName} {GetVisitedUserInfo.data.user.lastName}</h2>
+        <h3>Username: {GetVisitedUserInfo.data.user.userName}</h3>
         {!checkFollowing() ? renderFollowBtn() : renderUnfollowBtn()}
         </div>
         <div className="followers-info">
-          <h4 onClick={toggleList}>{`${GetUserInfo.data.user.followers.length} Followers (click)`}</h4>
-          {renderFollowers(GetUserInfo.data.user.followers)}
+          <h4 onClick={toggleList}>{`${GetVisitedUserInfo.data.user.followers.length} Followers (click)`}</h4>
+          {renderFollowers(GetVisitedUserInfo.data.user.followers)}
         </div>
       </section>
       <div className="user-profile-navigation">
         <button>Posts</button>
         <button>About</button>
       </div>
-      {renderPosts(GetUserInfo.data.user.posts)}
+      {renderPosts(GetVisitedUserInfo.data.user.posts)}
     </div>
   }
 
