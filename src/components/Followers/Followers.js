962b9ba47a3b9d522a7534b9ec0c66f4e5c1c975
Bobby Vasquez <shootbv@gmail.com>
2021-09-04 20:10:40 -0600

    Add current user following details

--- src/components/Followers/Followers.js
+++ src/components/Followers/Followers.js
@@ -1,18 +1,36 @@
 import { useQuery, useMutation } from "@apollo/client";
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import FollowerDetails from './FollowerDetails/FollowerDetails'
+import FollowingDetails from './FollowingDetails/FollowingDetails'
 import './Followers.css'
 
 import * as gql from '../../queries/queries'
 import UserContext from "../UserProfile/UserContext";
 
 const Followers = () => {
-  const value = useContext(UserContext)
 
+
+  const value = useContext(UserContext)
+  const GetFollowingInfo = useQuery(gql.GET_FOLLOWING_INFO(value))
   const { loading, error, data } = useQuery(gql.GET_USER_INFO(value))
   const [createFollower] = useMutation(gql.CREATE_FOLLOWER, {
-    refetchQueries: [{ query: gql.GET_USER_INFO(value) }],
+    refetchQueries: [{ query: gql.GET_USER_INFO(value) }, { query: gql.GET_FOLLOWING_INFO(value) }],
   });
+
+  const [userData, setUserData] = useState({})
+
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      if (GetFollowingInfo.data && data) {
+        setUserData({
+          userData: data,
+          following: GetFollowingInfo.data
+        })
+      }
+    }
+    return () => mounted = false;
+  }, [])
 
   const check = () => {
     if (data) console.log(data)
@@ -22,6 +40,15 @@ const Followers = () => {
     if (data) return data.user.followers.map((follower, index) => {
       return <div key={index}>
         <FollowerDetails id={follower.friendId} />
+      </div>
+    })
+  }
+
+  const renderFollowing = () => {
+    // console.log(GetFollowingInfo.data.usersFollowers)
+    return GetFollowingInfo.data.userFollowing.map((follower, index) => {
+      return <div key={index}>
+        <FollowerDetails id={follower.id} />
       </div>
     })
   }
@@ -42,9 +69,10 @@ const Followers = () => {
   return (
     <div>
     <h2>Fixed Followers</h2>
-      <p>Current user: ID{value}</p>
+      <h3>Followers for Current user ID{value}</h3>
       {!!data && renderFollowers()}
-      {<button onClick={e => addFriend(e)}>Add friend 1 (first last)</button>}
+      <h3>Following</h3>
+      {!!GetFollowingInfo.data && renderFollowing()}
     </div>
   )
 }
