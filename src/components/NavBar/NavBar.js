e2e708492e8ae7478c03529c04aa2ff515849ef9
Bobby Vasquez <shootbv@gmail.com>
2021-09-04 20:11:21 -0600

    Update navbar styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = ({newPost, setNewPost}) => {
         <Link exact to="/"><li><FontAwesomeIcon icon="home" /></li></Link>
         <Link exact to="/search-page"><li><FontAwesomeIcon icon="search" /></li></Link>
         <li className={(!newPost) ? 'new-post' : 'yellow'}><button onClick={togglePost} onKeyPress={handleKeypress}><FontAwesomeIcon icon="plus" /></button></li>
-        <Link exact to="/followers"><li><img src="https://img.icons8.com/dotty/80/000000/sine.png"/></li></Link>
+        <Link exact to="/followers"><li><FontAwesomeIcon icon="atom" /></li></Link>
         <Link exact to="/profile"><li><FontAwesomeIcon icon={['far', 'user-circle']} /></li></Link>
       </ul>
     </footer>
