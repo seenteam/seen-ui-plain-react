c39881f2f336a107f0621ada5b8f5d371b10a70d
Bobby Vasquez <shootbv@gmail.com>
2021-09-05 19:04:57 -0600

    Add functionality to like/unlike a post

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,79 @@
 import './Post.css'
+import React, { useState, useEffect } from 'react'
+import { useQuery, useMutation } from "@apollo/client";
+import Loading from '../Loading/Loading.js'
+import * as gql from '../../queries/queries'
 const dayjs = require('dayjs')
 const LocalizedFormat = require('dayjs/plugin/localizedFormat')
 dayjs.extend(LocalizedFormat)
 
-const Post = ({content, created}) => {
+const Post = ({id, content, created, currentUser }) => {
+  const [clicked, setClicked] = useState(false)
+
+  const GetPostLikes = useQuery(gql.GET_POST_LIKES(id))
+
+  useEffect(() => {
+    setClicked(false)
+  }, [GetPostLikes.data])
+
+  const [likePost] = useMutation(gql.CREATE_LIKE, {
+    refetchQueries: [{ query: gql.GET_POST_LIKES(id) }],
+  });
+
+  const [unlikePost] = useMutation(gql.DELETE_LIKE, {
+    refetchQueries: [{ query: gql.GET_POST_LIKES(id) }],
+  });
+
+  if (GetPostLikes.loading) {
+    return <Loading loading={GetPostLikes.loading} />
+  }
+  else {
+    console.log('LIKES FOR POST', id, GetPostLikes.data)
+  }
+
+  const checkLikes = () => {
+    // console.log('All post likes', GetPostLikes.data.postLikes, 'current id')
+    let found = GetPostLikes.data.postLikes.find(like => like.id === currentUser)
+    if (found) {
+      return true
+    }
+    return false
+  }
+
+  const like = () => {
+    setClicked(true)
+    likePost({
+      variables: {
+        'userId': currentUser,
+        'postId': id
+      },
+    })
+  }
+
+  const unlike = () => {
+    return
+  }
+
+  const renderLikeBtn = () => {
+    return <button disabled={clicked} onClick={like}>Like</button>
+  }
+
+  const renderUnLikeBtn = () => {
+    return <button disabled={clicked} onClick={unlike}>Unlike</button>
+  }
+
   return (
     <div className="post-container">
       <p><strong>{content}</strong></p>
       <p>Posted {dayjs(created).format('LLL')}</p>
       <div className="likes-container">
-        <p># Likes</p>
-        <button>Like</button>
+        <p>{!!GetPostLikes.data && `${GetPostLikes.data.postLikes.length} Likes`}</p>
+        <section>
+          {!!(id !== currentUser) && <section>
+
+            {!checkLikes() ? renderLikeBtn() : renderUnLikeBtn()}
+            </section>}
+        </section>
       </div>
     </div>
   )
