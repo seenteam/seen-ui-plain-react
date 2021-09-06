c0e8cc2b0a7caa92d181540e56b451dc0d673b43
Bobby Vasquez <shootbv@gmail.com>
2021-09-06 00:48:35 -0600

    Render Flux followers/following

--- src/components/Followers/Followers.js
+++ src/components/Followers/Followers.js
@@ -11,6 +11,8 @@ const Followers = () => {
 
   const value = useContext(UserContext)
   const GetFollowingInfo = useQuery(gql.GET_FOLLOWING_INFO(value))
+  const GetFluxFollowing = useQuery(gql.GET_USER_FLUX_FOLLOWING(value))
+  const GetFluxFollowers = useQuery(gql.GET_USER_FLUX_FOLLOWERS(value))
   const { loading, error, data } = useQuery(gql.GET_USER_INFO(value))
   const [createFollower] = useMutation(gql.CREATE_FOLLOWER, {
     refetchQueries: [{ query: gql.GET_USER_INFO(value) }, { query: gql.GET_FOLLOWING_INFO(value) }],
@@ -52,6 +54,23 @@ const Followers = () => {
     })
   }
 
+  const renderFluxFollowers = () => {
+    if (GetFluxFollowers.data) return GetFluxFollowers.data.usersFluxFollowers.map((follower, index) => {
+      return <div key={index}>
+        <FollowerDetails id={follower.friendId} />
+      </div>
+    })
+  }
+
+  const renderFluxFollowing = () => {
+    // console.log(GetFollowingInfo.data.usersFollowers)
+    return GetFluxFollowing.data.userFluxFollowing.map((follower, index) => {
+      return <div key={index}>
+        <FollowerDetails id={follower.id} />
+      </div>
+    })
+  }
+
   const addFriend = (e) => {
     let idToAdd = 1
     if (idToAdd === value || data.user.followers.find(follower => follower.friendId === idToAdd)) return e.target.innerText = 'Cant add!'
@@ -66,13 +85,22 @@ const Followers = () => {
   }
 
   return (
-    <div>
-    <h2>Fixed Followers</h2>
-      <h3>Followers for Current user ID{value}</h3>
-      {!!data && renderFollowers()}
-      <h3>Following</h3>
-      {!!GetFollowingInfo.data && renderFollowing()}
-    </div>
+    <section className="followers-containers">
+      <div>
+        <h2>Fixed</h2>
+        <h3>Followers</h3>
+        {!!data && renderFollowers()}
+        <h3>Following</h3>
+        {!!GetFollowingInfo.data && renderFollowing()}
+      </div>
+      <div>
+        <h2>Flux</h2>
+        <h3>Followers</h3>
+        {!!GetFluxFollowers.data && renderFluxFollowers()}
+        <h3>Following</h3>
+        {!!GetFluxFollowing.data && renderFluxFollowing()}
+      </div>
+    </section>
   )
 }
 
