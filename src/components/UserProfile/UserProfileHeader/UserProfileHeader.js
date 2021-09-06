0d87164f15963357eb2dd61ca6b862217bf4367b
Bobby Vasquez <shootbv@gmail.com>
2021-09-06 00:52:55 -0600

    Add component to clean up UserProfile component

---
+++ src/components/UserProfile/UserProfileHeader/UserProfileHeader.js
@@ -0,0 +1,25 @@
+import './UserProfileHeader.css'
+
+const UserProfileHeader = ({ fname, lname, uname, checkFollowing, renderFBtn, renderUBtn, fixedFollowers, fluxFollowers, renderFixed, renderFlux, toggleList, toggleFluxList}) => {
+  return (
+    <section className="profile-header">
+      <div className="user-title">
+      <h2>{fname} {lname}</h2>
+      <h3>@{uname}</h3>
+      {!checkFollowing() ? renderFBtn() : renderUBtn()}
+      </div>
+      <div className="followers-info" onClick={toggleList}>
+        <h4>{fixedFollowers.length}</h4>
+        <p>Fixed {fixedFollowers.length === 0 || fixedFollowers.length > 1 ? 'Followers' : 'Follower'}</p>
+        {renderFixed(fixedFollowers)}
+      </div>
+      <div className="flux-followers-info" onClick={toggleFluxList}>
+          <h4>{fluxFollowers.length}</h4>
+          <p>Flux {fluxFollowers.length === 0 || fluxFollowers.length > 1 ? 'Followers' : 'Follower'}</p>
+        {renderFlux(fluxFollowers)}
+      </div>
+    </section>
+  )
+}
+
+export default UserProfileHeader
