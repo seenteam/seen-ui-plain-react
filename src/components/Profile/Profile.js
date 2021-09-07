2b8798a0cdf5853bfd9abc44d30e7f0a3d1683d5
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-06 19:37:17 -0600

    Remove unnecessary components from profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,39 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
+import UserContext from '../UserProfile/UserContext';
 import './Profile.css'
 import ProfileDetails from './ProfileDetails/ProfileDetails';
 import UpdateUserData from './UpdateUserData/UpdateUserData';
 
 // Query Posts from DB that correspond to current User
 
-const Profile = ({user, posts, setNewPost}) => {
+const Profile = () => {
   
   useEffect(() => {
     let mounted = true;
-    if (mounted) setNewPost(false)
     return () => mounted = false;
   }, [])
 
-  const userProfile = () => {
-    return (
-      <div>
-        <h2>{user.username}</h2>
-        <h3>{user.first_name} {user.last_name}</h3>
-        <h4>Joined {user.created_at}</h4>
-        <section>
-          <h2>My Posts</h2>
-          <div>
-            {posts.map((post, index) => <p key={index}>{post}</p>)}
-          </div>
-        </section>
-      </div>
-      )
-  }
-
   return (
     <div className="user-profile">
-      {!user ? <h2>Please log in</h2> : userProfile()}
       <ProfileDetails />
-      <button onClick > Update Profile </button>
       <UpdateUserData />
     </div>
   )
