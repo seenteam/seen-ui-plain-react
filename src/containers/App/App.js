44a8c0372e563ffc3e1c2b1c060936eacf6fe894
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-06 23:14:59 -0600

    Refactor app.js Routes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -10,10 +10,8 @@ import UserProfile from '../../components/UserProfile/UserProfile'
 import {Switch, Route} from 'react-router-dom'
 import Header from '../../components/Header/Header.js'
 import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fab, fas, far } from '@fortawesome/free-brands-svg-icons'
 import { faAtom, faHome, faSearch, faUserCircle as faUserCircleActive, faPlus, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import { faUserCircle } from '@fortawesome/free-regular-svg-icons'
-// import { faUserCircle } from '@fortawesome/free-regular-svg-icons';
 import UserContext from '../../components/UserProfile/UserContext.js'
 import './App.css';
 library.add(faAtom, faHome, faSearch, faUserCircle, faUserCircleActive, faPlus, faChevronRight);
@@ -35,34 +33,28 @@ const App = () => {
     <UserContext.Provider value={userID}>
       <div>
         <Switch>
-          <Route exact path="/" render={() => {
-            return <section>
-                    <Header
-                      setNewPost={setNewPost}
-                    />
-                    <UserPosts/>
-                    </section>
-                  }}
-                />
-              <Route exact path="/profile" render={() => {
-                  return <UserProfile user={userID} />
-                }}
-              />
-              <Route exact path="/profile/edit" render={() => {
-                    return <Profile />
-              }}/>
-              <Route exact path="/search-page">
-                <SearchPage
-                  setNewPost={setNewPost}
-                />
-              </Route>
-              <Route exact path="/followers" render={() => {
-                  return <Followers />
-                }}/>
-              <Route exact path="/users/:id" render={({match}) => {
-                    const { id } = match.params
-                    return <UserProfile user={id} />
-              }}/>
+          <Route exact path="/" >
+            <section>
+              <Header setNewPost={setNewPost}/>
+              <UserPosts/>
+            </section>
+          </Route>
+          <Route exact path="/profile" >
+            <UserProfile user={userID} />
+          </Route>     
+          <Route exact path="/profile/edit" >
+            <Profile />
+          </Route>      
+          <Route exact path="/search-page">
+            <SearchPage setNewPost={setNewPost}/>
+          </Route>
+          <Route exact path="/followers" >
+            <Followers />
+          </Route>
+          <Route exact path="/users/:id" render={({match}) => {
+              const { id } = match.params
+              return <UserProfile user={id} />
+          }}/>
         </Switch>
         <WavesLayer />
         <NewPost
