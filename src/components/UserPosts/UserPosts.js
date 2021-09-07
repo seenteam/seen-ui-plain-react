80431c9c30ef1c3839add3e2eb7d18155ca778f8
Bobby Vasquez <shootbv@gmail.com>
2021-09-06 18:44:02 -0600

    Update UsersPosts with delete button inside of post

--- src/components/UserPosts/UserPosts.js
+++ src/components/UserPosts/UserPosts.js
@@ -53,11 +53,15 @@ const UserPosts = () => {
      <section className="posts-container">
        <h2>Feed</h2>
         {!!data && [...data.user.posts].sort((a, b) => parseInt(b.id) - parseInt(a.id)).map((post, index) => <div key={index}>
-          <Post id={post.id} content={post.content} created={post.createdAt} currentUser={value} user={value} />
-
-          <button
-            disabled={clicked}
-            onClick={() => removePost(post.id)}>Delete Post</button>
+          <Post
+            id={post.id}
+            content={post.content}
+            created={post.createdAt}
+            currentUser={value} 
+            user={value}
+            clickedDelete={clicked}
+            removePost={removePost}
+            />
         </div>)}
      </section>
    )
