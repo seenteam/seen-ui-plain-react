be3a1dfa8e9dc3cc9539b2580cfa1ee138b86653
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-07 17:16:53 -0600

    Start setup of the edit_user_profile page

---
+++ cypress/integration/edit_user_profile_spec.js
@@ -0,0 +1,16 @@
+/// <reference types="Cypress" />
+
+describe("Edit User Profile Page Spec", () => {
+
+    beforeEach(() => {
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql').as('request') 
+        cy.visit('http://localhost:3000/profile/edit');
+        cy.wait('@request');
+    })
+
+    it('Should contain the string "Aplus"',  () => {
+        cy.contains('Aplus')
+    })
+
+})
+
