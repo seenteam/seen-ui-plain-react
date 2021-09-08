edf29b8cdfe7c1f7e66a428104358ffac1355ec0
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-07 18:52:56 -0600

    Add the start to testing for the userProfile page

--- cypress/integration/edit_user_profile_spec.js
+++ cypress/integration/edit_user_profile_spec.js
@@ -3,7 +3,10 @@
 describe("Edit User Profile Page Spec", () => {
 
     beforeEach(() => {
-        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql').as('request') 
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', {
+            statusCode: 200,
+            fixture: 'userProfile.json'
+        }).as('request') 
         cy.visit('http://localhost:3000/profile/edit');
         cy.wait('@request');
     })
