ebcbdefdeac2a3aef1536ccc4443a52ccaf7403d
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-07 18:53:31 -0600

    Utilize helper methods to check for the alias

--- cypress/integration/edit_user_profile_spec.js
+++ cypress/integration/edit_user_profile_spec.js
@@ -1,14 +1,20 @@
 /// <reference types="Cypress" />
+import { aliasQuery, aliasMutation } from '../../src/utilities/graphql-test-utils';
 
 describe("Edit User Profile Page Spec", () => {
 
     beforeEach(() => {
-        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', {
-            statusCode: 200,
-            fixture: 'userProfile.json'
-        }).as('request') 
+        // cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', {
+        //     statusCode: 200,
+        //     fixture: 'userProfile.json'
+        // }).as('request') 
+
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+            aliasQuery(req, req.body.operationName)
+            req.reply({ fixture: 'userProfile.json'});
+          })
         cy.visit('http://localhost:3000/profile/edit');
-        cy.wait('@request');
+        cy.wait('@gqlGetUserInfoQuery');
     })
 
     it('Should contain the string "Aplus"',  () => {
