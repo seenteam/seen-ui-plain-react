38622bb2c7fb1c0ce05735f18908d7500ce5e869
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-08 02:12:35 -0600

    Write tests to open, close, & input into post modal

--- cypress/integration/post_spec.js
+++ cypress/integration/post_spec.js
@@ -79,16 +79,28 @@ describe("User Post Spec", () => {
     })
 
     
-    it("Should be able to open the post modal",  () => { 
+    it("Should be able to open the post modal",  () => {
+        cy.get('.new-post > button > .svg-inline--fa > path').click()
+        cy.contains("Make a new Post").should('have.text', 'Make a new Post')
 
     })
 
     it("Should be able to close the post modal",  () => { 
 
+        cy.get('.new-post > button > .svg-inline--fa > path').click()
+        cy.contains("Make a new Post").should('have.text', 'Make a new Post')
+        cy.get('.yellow > button > .svg-inline--fa > path').click()
+        cy.contains("Viral Users").should('have.text', 'Viral Users')
     })
 
 
     it("Should be able to type up to 70 chars",  () => { 
+        cy.get('.new-post > button > .svg-inline--fa > path').click()
+        cy.contains("Make a new Post").should('have.text', 'Make a new Post')
+
+        cy.contains("70").should('have.text', 'chars left: 70/70')
+        cy.get('textarea').type('VNM5ZkJsmyEYm5yyeva68oAbwmLY58fDpBA96xgYlDN0hnB3qV6N9dmvB08Rnf7rn3KT5r')
+        cy.get('.char-counter').should('have.text', 'chars left: 0/70')
 
     })
 
