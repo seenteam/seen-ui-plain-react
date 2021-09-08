135ad6f4196780f881bc0d36004d869a8dd21726
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-07 18:54:00 -0600

    remove old fixtures method

--- cypress/integration/edit_user_profile_spec.js
+++ cypress/integration/edit_user_profile_spec.js
@@ -4,11 +4,6 @@ import { aliasQuery, aliasMutation } from '../../src/utilities/graphql-test-util
 describe("Edit User Profile Page Spec", () => {
 
     beforeEach(() => {
-        // cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', {
-        //     statusCode: 200,
-        //     fixture: 'userProfile.json'
-        // }).as('request') 
-
         cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
             aliasQuery(req, req.body.operationName)
             req.reply({ fixture: 'userProfile.json'});
