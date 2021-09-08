ec3f4857015f9bd1d8fc411e98eb6f6e4b884f17
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-09-08 01:16:38 -0600

    Add some basic fixtures

--- cypress/integration/edit_user_profile_spec.js
+++ cypress/integration/edit_user_profile_spec.js
@@ -4,15 +4,25 @@ import { aliasQuery, aliasMutation } from '../../src/utilities/graphql-test-util
 describe("Edit User Profile Page Spec", () => {
 
     beforeEach(() => {
+        // cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+        //     aliasQuery(req, req.body.operationName)
+        //     req.reply({ fixture: 'userProfile.json'});
+        //   })
+        // cy.visit('http://localhost:3000/profile/edit');
+        // cy.wait('@gqlGetUserInfoQuery');
+    })
+
+    it("Should contain the test user Aplus AceAttorney's info ",  () => {
+
+        ///
         cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
             aliasQuery(req, req.body.operationName)
             req.reply({ fixture: 'userProfile.json'});
           })
         cy.visit('http://localhost:3000/profile/edit');
         cy.wait('@gqlGetUserInfoQuery');
-    })
+        ////
 
-    it("Should contain the test user Aplus AceAttorney's info ",  () => {
         cy.contains('testy').should('be.visible')
         cy.contains('Aplus').should('be.visible')
         cy.contains('AceAttorney').should('be.visible')
@@ -22,6 +32,15 @@ describe("Edit User Profile Page Spec", () => {
     })
     
     it('Should not allow the user to submit a form with incomplete fields in the form!', () => {
+
+        ////
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+            aliasQuery(req, req.body.operationName)
+            req.reply({ fixture: 'userProfile.json'});
+          })
+        cy.visit('http://localhost:3000/profile/edit');
+        cy.wait('@gqlGetUserInfoQuery');
+        //////
 
         cy.get('input:invalid').should('have.length', 6)
 
@@ -41,6 +60,16 @@ describe("Edit User Profile Page Spec", () => {
     })
 
     it('Should allow the user to type in the form!', () => {
+        ////
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+            aliasQuery(req, req.body.operationName)
+            req.reply({ fixture: 'userProfile.json'});
+          })
+        cy.visit('http://localhost:3000/profile/edit');
+        cy.wait('@gqlGetUserInfoQuery');
+
+        ///
+
         cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
             aliasMutation(req, req.body.operationName)
             req.reply({ fixture: 'userProfileUpdate.json'});
@@ -57,12 +86,67 @@ describe("Edit User Profile Page Spec", () => {
         cy.wait('@gqlupdateUserMutation');
     })
 
-    it('Should allow the user to return to the user-profile when the return to profile button is clicked!', () => {
-        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
-            aliasMutation(req, req.body.operationName)
-            req.reply({ fixture: 'userProfileUpdate.json'});
-          })
+    it.only('Should allow the user to return to the user-profile when the return to profile button is clicked!', () => {
         
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', req => {
+          if (req.body.operationName === 'GetUserInfo') {
+            req.alias = 'userQuery';
+            req.reply({
+              body: {
+                data: {
+                  user: {
+                    id: 2,
+                    firstName: "Aplus",
+                    lastName: "Attorney",
+                    userName: "user1",
+                    __typename: "User",
+                    followers: [],
+                    posts: [
+                      {
+                        id: 1,
+                        content: "First post!",
+                        createdAt: "2021-09-05T21:54:58Z"
+                      }
+                    ],
+                  }
+                }
+              },
+              headers: {
+                'access-control-allow-origin': '*',
+              }
+            })
+          }
+        });
+    
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+            if (req.body.operationName === 'getUserInfo') {
+                req.alias = 'followerInfo';
+                req.reply({
+                  body: {
+                    "data":{"usersFollowers":[]}
+            },
+            headers: {
+                'access-control-allow-origin': '*',
+              }
+            })
+          }
+        });
+    
+        cy.intercept('POST', 'https://intense-ocean-61260.herokuapp.com/graphql', (req) => {
+            if (req.body.operationName === 'getUserInfo') {
+                req.alias = 'fluxFollowerInfo';
+                req.reply({
+                  body: {
+                    "data":{"usersFluxFollowers":[]}
+            } , headers: {
+                'access-control-allow-origin': '*',
+              }
+            })
+          }
+        });
+
+        cy.visit('http://localhost:3000/profile/edit');
+        cy.get('a > button').click();
     })
 
 })
