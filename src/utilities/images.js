7199c140096d028576ca31c0bba0c6bb794706bf
Bobby Vasquez <shootbv@gmail.com>
2021-09-07 18:37:33 -0600

    Import / export seen logo

--- src/utilities/images.js
+++ src/utilities/images.js
@@ -1,3 +1,8 @@
 //Shayan's wave
 import waves from '../assets/svgs/purp-wave.svg';
+import logo from '../assets/pngs/seen-logo.png';
 export const svgs = [waves]
+
+export const seenLogo = logo
+
+// export default seenLogo
