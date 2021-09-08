f7887be60458242e234c9ec1eba0f45a40220e4f
Bobby Vasquez <shootbv@gmail.com>
2021-09-07 18:33:54 -0600

    Render fixed/flux following posts to User Feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,34 @@
-
+import { useQuery, useMutation } from "@apollo/client";
+import Loading from '../Loading/Loading.js'
+import Post from '../Post/Post.js'
+import * as gql from '../../queries/queries'
 import './Feed.css'
-const Feed = () => {
-  return (
-    <div>
 
-    </div>
+
+const Feed = ({ user }) => {
+
+  const UserPosts = useQuery(gql.GET_USER_POSTS(user))
+  const GetPostsFromFollowing = useQuery(gql.GET_POSTS_FROM_FOLLOWING(user))
+
+
+  const renderAllPosts = () => {
+    if (UserPosts.data && GetPostsFromFollowing.data) {
+      let relevantPosts = [...UserPosts.data.user.posts, ...GetPostsFromFollowing.data.getPostFromFixedFollowing, ...GetPostsFromFollowing.data.getPostFromFluxFollowing]
+      let uniquePosts = [...new Set(relevantPosts)]
+      let limited = uniquePosts.sort((a, b) => a.createdAt > b.createdAt ? 1 : -1).slice(0, 40)
+      return limited.map(post => <Post
+        id={post.id}
+        content={post.content}
+        created={post.createdAt}
+        user={user}
+        />)
+    }
+  }
+
+  return (
+    <section className="feed-container">
+    {renderAllPosts()}
+    </section>
   )
 }
 
